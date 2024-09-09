4200cd66c9cf376927329ac0da241d488c4aad87
Eigil Sagafos <eigil@shiftx.com>
2024-09-09 12:39:40 -0400

    v0.1.0-alpha.0

--- packages/react/src/useResetValdresState.ts
+++ packages/react/src/useResetValdresState.ts
@@ -1,12 +1,8 @@
 import { useCallback } from "react"
-import type { Atom } from "@valdres/core"
 import { useValdresStore } from "./useValdresStore"
-// import { initAtom } from "./lib/initAtom"
+import type { Atom } from "@valdres/core"
 
 export const useResetValdresState = <V>(atom: Atom<V>): (() => void) => {
     const store = useValdresStore()
-    return useCallback(() => {
-        store.reset(atom)
-        // initAtom(atom, store.data)
-    }, [atom])
+    return useCallback(() => store.reset(atom), [atom, store])
 }
