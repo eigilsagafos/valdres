807b30b11b127552246f8b9140ad236b6d36fa37
Eigil Sagafos <eigil@shiftx.com>
2024-09-24 17:16:22 -0700

    Docs structure

--- packages/core/documentation/vocs.config.ts
+++ packages/core/documentation/vocs.config.ts
@@ -20,39 +20,102 @@ export default defineConfig({
     topNav: [
         {
             text: "React",
-            link: "/docs/valdres-react",
-            match: "/docs/valdres-react/",
+            link: "/docs/react",
+            match: "/docs/react/",
         },
-        { text: "Vanilla JS", link: "/docs/valdres", match: "/docs/valdres/" },
+        { text: "Vanilla JS", link: "/docs/vanilla", match: "/docs/vanilla/" },
     ],
-    sidebar: [
-        {
-            text: "Getting Started",
-            link: "/getting-started",
-        },
-        {
-            text: "Example",
-            link: "/example",
-        },
-        {
-            text: "API",
-            collapsed: true,
-            items: [
-                {
-                    text: "Config",
-                    link: "/docs/api/config",
-                },
-                {
-                    text: "API",
-                    collapsed: true,
-                    items: [
-                        {
-                            text: "Config",
-                            link: "/docs/api/config",
-                        },
-                    ],
-                },
-            ],
-        },
-    ],
+    sidebar: {
+        "/docs/vanilla": [
+            {
+                text: "Getting Started",
+                link: "/getting-started",
+            },
+            {
+                text: "About",
+                link: "/example",
+            },
+            {
+                text: "API",
+                collapsed: false,
+                items: [
+                    {
+                        text: "atom",
+                        link: "/docs/vanilla/api/atom",
+                    },
+                    {
+                        text: "selector",
+                        link: "/docs/vanilla/api/selector",
+                    },
+                ],
+            },
+        ],
+        "/docs/react": [
+            {
+                text: "Getting Started",
+                link: "/getting-started",
+            },
+            {
+                text: "Example",
+                link: "/example",
+            },
+            {
+                text: "API",
+                collapsed: false,
+                items: [
+                    {
+                        text: "atom",
+                        link: "/docs/react/api/atom",
+                    },
+                    {
+                        text: "selector",
+                        link: "/docs/react/api/selector",
+                    },
+                    {
+                        text: "useValdresValue",
+                        link: "/docs/react/api/useValdresValue",
+                    },
+                    {
+                        text: "useValdresState",
+                        link: "/docs/react/api/useValdresState",
+                    },
+                    {
+                        text: "useSetValdresState",
+                        link: "/docs/react/api/useSetValdresState",
+                    },
+                    {
+                        text: "useResetValdresState",
+                        link: "/docs/react/api/useResetValdresState",
+                    },
+                    {
+                        text: "useValdresStore",
+                        link: "/docs/react/api/useValdresStore",
+                    },
+                    {
+                        text: "ValdresProvider",
+                        link: "/docs/react/api/ValdresProvider",
+                    },
+                ],
+            },
+            {
+                text: "Packages",
+                collapsed: false,
+                items: [
+                    {
+                        text: "@valdres-react/color-mode",
+                        collapsed: true,
+                        items: [
+                            {
+                                text: "Getting Started",
+                                link: "/docs/react/api/useValdresValue",
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
 })
+// export { useValdresCallback } from "./src/useValdresCallback"
+// export { useValdresValueWithDefault } from "./src/useValdresValueWithDefault"
+// export { ValdresProvider } from "./src/ValdresProvider"
