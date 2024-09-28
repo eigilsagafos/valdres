6f62b1255f832d079611964b46bfd00d1ab0595b
Eigil Sagafos <eigil@shiftx.com>
2024-09-28 11:58:05 -0700

    @valdres/color-mode: cleanup

--- @valdres/color-mode/src/colorModeAtom.ts
+++ @valdres/color-mode/src/colorModeAtom.ts
@@ -11,20 +11,4 @@ export const colorModeAtom = atom<ColorMode>(getSystemColorMode, {
                 setSelf(getSystemColorMode())
             })
     },
-    // onMount: setSelf => {
-    //     const callback = event => setSelf(event.matches)
-    //     const mediaWatcher = window?.matchMedia(query)
-    //     if (mediaWatcher) {
-    //         mediaWatcher.addEventListener("change", callback)
-    //         return [mediaWatcher, callback] as [MediaQueryList, () => void]
-    //     }
-    // },
-    // onUnmount: data => {
-    //     if (data) {
-    //         const [mediaWatcher, callback] = data
-    //         mediaWatcher.removeEventListener("change", callback)
-    //     }
-    //     // window?.matchMedia(query).removeEventListener("change")
-    //     // console.log("onUnmount", asdf)
-    // },
 })
