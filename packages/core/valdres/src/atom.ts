b488c2e96896b8fee8323f1e4884452081c68d71
Eigil Sagafos <eigil@shiftx.com>
2024-10-13 20:43:48 -0700

    valdres: improve atom types

--- packages/core/valdres/src/atom.ts
+++ packages/core/valdres/src/atom.ts
@@ -1,17 +1,29 @@
 import { globalAtom } from "./lib/globalAtom"
-import type { Atom } from "./types/Atom"
+import type { Atom, GlobalAtom } from "./types/Atom"
+import type { AtomDefaultValue } from "./types/AtomDefaultValue"
 import type { AtomOptions } from "./types/AtomOptions"
-import type { Selector } from "./types/Selector"
 
-export const atom = <Value = any, FamilyKey = undefined>(
-    defaultValue?: Value | (() => Value | Promise<Value>) | Selector<Value>,
-    options?: AtomOptions<Value>,
-): Atom<Value, FamilyKey> => {
+/** [Docs Reference](https://valdres.dev/valdres/api/atom) */
+export function atom<V>(
+    defaultValue: AtomDefaultValue<V>,
+    options: AtomOptions<V> & { global: true },
+): GlobalAtom<V>
+
+export function atom<V>(
+    defaultValue: AtomDefaultValue<V>,
+    options?: AtomOptions<V>,
+): Atom<V>
+
+export function atom<V>(
+    defaultValue: AtomDefaultValue<V>,
+    options?: AtomOptions<V>,
+) {
     if (!options) return { defaultValue }
-    // @ts-ignore @ts-todo
-    if (options.global) return globalAtom(defaultValue, options)
+    if (options.global) {
+        return globalAtom(defaultValue, options)
+    }
     return {
         defaultValue,
         ...options,
-    } as Atom<Value, FamilyKey>
+    } as Atom<V>
 }
