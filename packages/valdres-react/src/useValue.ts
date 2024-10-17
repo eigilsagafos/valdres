c7ba4ba73394cd6cdd297354aa000bc444922417
Eigil Sagafos <eigil@shiftx.com>
2024-10-17 14:30:30 -0700

    Improve package structure

--- packages/valdres-react/src/useValue.ts
+++ packages/valdres-react/src/useValue.ts
@@ -2,13 +2,11 @@ import { isPromiseLike, type State, type Store } from "valdres"
 import { useSyncExternalStore } from "react"
 import { useStore } from "./useStore"
 
-export const useValue = (state: State, store?: Store) => {
+export const useValue = <V>(state: State<V>, store?: Store) => {
     const currentStore = store || useStore()
     const res = useSyncExternalStore(
         cb => currentStore.sub(state, cb, false),
-        // @ts-ignore @ts-todo
         () => currentStore.get(state),
-        // @ts-ignore @ts-todo
         () => currentStore.get(state),
     )
     if (isPromiseLike(res)) {
