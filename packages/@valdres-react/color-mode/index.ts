9d3cafb3cbd6f167f64b230fb35378fc4cfb99b5
Eigil Sagafos <eigil@shiftx.com>
2024-11-01 08:58:02 -0700

    @valdres-react/color-mode: update to reflect changes in @valdres/color-mode

--- packages/@valdres-react/color-mode/index.ts
+++ packages/@valdres-react/color-mode/index.ts
@@ -1,3 +1,13 @@
+export {
+    colorModeSelector,
+    getSystemColorMode,
+    isDarkMode,
+    isLightMode,
+    systemColorModeAtom,
+    userSelectedColorModeAtom,
+    type ColorMode,
+    type UserSelectedColorMode,
+} from "@valdres/color-mode"
 export { useColorMode } from "./src/useColorMode"
 export { useIsDarkMode } from "./src/useIsDarkMode"
 export { useIsLightMode } from "./src/useIsLightMode"
--- packages/@valdres-react/color-mode/src/useColorMode.ts
+++ packages/@valdres-react/color-mode/src/useColorMode.ts
@@ -1,4 +1,4 @@
-import { colorMode } from "@valdres/color-mode"
+import { colorModeSelector } from "@valdres/color-mode"
 import { useValue } from "valdres-react"
 
-export const useColorMode = () => useValue(colorMode)
+export const useColorMode = () => useValue(colorModeSelector)
