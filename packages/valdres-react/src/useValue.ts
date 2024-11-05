219dc3d9961b6dd8f8dcee66c4501e753da2e1b6
Eigil Sagafos <eigil@shiftx.com>
2024-11-05 09:16:26 -0800

    valdres: Fix performance issue in useValue by memoizing callback function

--- packages/valdres-react/src/useValue.ts
+++ packages/valdres-react/src/useValue.ts
@@ -1,14 +1,20 @@
 import { isPromiseLike, type State, type Store } from "valdres"
-import { useSyncExternalStore } from "react"
+import { useCallback, useSyncExternalStore } from "react"
 import { useStore } from "./useStore"
 
 export const useValue = <V>(state: State<V>, store?: Store) => {
     const currentStore = store || useStore()
-    const res = useSyncExternalStore(
-        cb => currentStore.sub(state, cb, false),
-        () => currentStore.get(state),
-        () => currentStore.get(state),
+    const subscribe = useCallback(
+        (cb: () => void) => {
+            return currentStore.sub(state, cb, false)
+        },
+        [state, currentStore],
     )
+    const getSnapshot = useCallback(
+        () => currentStore.get(state),
+        [state, currentStore],
+    )
+    const res = useSyncExternalStore(subscribe, getSnapshot, getSnapshot)
     if (isPromiseLike(res)) {
         throw res
     } else {
