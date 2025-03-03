7dda767d0e889bea9c6dcecfcf6a24ef72e3944e
Eigil Sagafos <eigil@shiftx.com>
2025-03-03 08:46:34 -0800

    Deep freeze debug:

---
+++ packages/valdres/src/lib/isProd.ts
@@ -0,0 +1,4 @@
+export const isProd = () => {
+    return false
+    // return process.env.NODE_ENV === "production"
+}
--- packages/valdres/src/lib/setValueInData.ts
+++ packages/valdres/src/lib/setValueInData.ts
@@ -1,15 +1,14 @@
 import type { Atom } from "../types/Atom"
 import type { StoreData } from "../types/StoreData"
 import { deepFreeze } from "../utils/deepFreeze"
-
-const isProd = process.env.NODE_ENV === "production"
+import { isProd } from "./isProd"
 
 export const setValueInData = <Value = any>(
     atom: Atom<Value>,
     value: Value,
     data: StoreData,
 ): Value => {
-    if (isProd) {
+    if (isProd()) {
         data.values.set(atom, value)
         return value
     } else {
